3460a91882ea77ab6f741840db806362c9ac48dd
ArtemYakushkin <zimberger29101985@gmail.com>
2022-06-03 20:53:42 +0300

    add second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryLightBox = document.querySelector('.gallery');
+const itemGallery = createGalletyItem(galleryItems);
+
+galleryLightBox.insertAdjacentHTML('beforeend', itemGallery);
+galleryLightBox.addEventListener('click', onPictureClick);
+
+function createGalletyItem(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `
+            <li>
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+                </a>
+            </li>`;
+    }).join('');
+};
+
+function onPictureClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    let lightbox = new SimpleLightbox('.gallery a', { 
+        preloading: false,
+        alertError: false
+     });
+    console.log(lightbox);
+}
